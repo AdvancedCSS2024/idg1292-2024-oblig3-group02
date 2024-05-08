a68c219b1528c8e3c9ff7dbc5a53a14b03ccf756
Hanaka15 <34461088+Hanaka15@users.noreply.github.com>
2024-05-08 23:46:01 +0200

    its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end. its never the end.

--- scripts/diver.js
+++ scripts/diver.js
@@ -1,51 +1,26 @@
-// Retrieve necessary elements from the DOM
 const main = document.querySelector('#main');
 const sprite = document.querySelector('#sprite');
 const playerbox = document.querySelector('#playerbox');
-const underwater = document.querySelector("#underwater");
 const colliders = document.querySelectorAll('.collider');
-let mouseOverMain = false
-let transform = 0; 
+const underwater = document.querySelector("#underwater");
+const gradientContainer = document.getElementById('gradient-container');
 
-// Define variables for movement and transformation
 let prevInputX = 0;
 let prevInputY = 0;
 
-function scrollInput() { 
-    const spriteRect = sprite.getBoundingClientRect();
-    const spriteCenterY = spriteRect.top + spriteRect.height / 2;
-    const windowHeight = window.innerHeight;
-    const scrollThreshold = window.innerHeight * 0.4;
-
-    // Scroll up if the sprite is above the threshold
-    if (spriteCenterY < scrollThreshold && mouseOverMain) {
-        window.scrollBy(0, -3);
-    }
-
-    // Scroll down if the sprite is below the threshold
-    if (spriteCenterY > windowHeight - scrollThreshold && mouseOverMain) {
-        window.scrollBy(0, 3);
-    }
-}
-
-function handleMouseMove(event) {
+const handleMouseMove = (event) => {
     const rect = main.getBoundingClientRect();
-    prevInputX = event.clientX - rect.left + Math.abs(transform);
+    prevInputX = event.clientX - rect.left;
     prevInputY = event.clientY - rect.top;
-}
+};
 
-// Function to handle touch movement
-function handleTouchMove(event) {
+const handleTouchMove = (event) => {
     const rect = main.getBoundingClientRect();
-    const touchX = event.touches[0].clientX - rect.left + Math.abs(transform);
-    const touchY = event.touches[0].clientY - rect.top;
+    prevInputX = event.touches[0].clientX - rect.left;
+    prevInputY = event.touches[0].clientY - rect.top;
+};
 
-    prevInputX = touchX;
-    prevInputY = touchY;
-}
-
-// Function to check for collisions between player and colliders
-function checkCollisions() {
+const checkCollisions = () => {
     const pRect = playerbox.getBoundingClientRect();
     const points = [
         { x: pRect.left, y: pRect.top },
@@ -54,111 +29,65 @@ function checkCollisions() {
         { x: pRect.left, y: pRect.bottom }
     ];
 
-    let xCollision = false;
-    let yCollision = false;
-    let xCollisionSide = null; // Side of x-axis collision ('left' or 'right')
-    let yCollisionSide = null; // Side of y-axis collision ('top' or 'bottom')
+    let collision = { x: false, y: false };
+    let collisionSide = { x: null, y: null };
     let colliderEl = null;
 
-    // Check collision for each collider
     for (const collider of colliders) {
         const colliderRect = collider.getBoundingClientRect();
-        colliderEl = collider;
         for (const point of points) {
-            if (
-                point.x >= colliderRect.left &&
-                point.x <= colliderRect.right &&
-                point.y >= colliderRect.top &&
-                point.y <= colliderRect.bottom
-            ) {
-                // Determine the side the player colided with.
+            if (isPointInsideElement(point, colliderRect)) {
                 const dx = Math.min(Math.abs(point.x - colliderRect.left), Math.abs(point.x - colliderRect.right));
                 const dy = Math.min(Math.abs(point.y - colliderRect.top), Math.abs(point.y - colliderRect.bottom));
                 if (dx < dy) {
-                    xCollision = true; // Collision along the x-axis
-                    xCollisionSide = point.x < (colliderRect.left + colliderRect.right) / 2 ? 'right' : 'left';
+                    collision.x = true;
+                    collisionSide.x = point.x < (colliderRect.left + colliderRect.right) / 2 ? 'right' : 'left';
                 } else {
-                    yCollision = true; // Collision along the y-axis
-                    yCollisionSide = point.y < (colliderRect.top + colliderRect.bottom) / 2 ? 'bottom' : 'top';
+                    collision.y = true;
+                    collisionSide.y = point.y < (colliderRect.top + colliderRect.bottom) / 2 ? 'bottom' : 'top';
                 }
+                colliderEl = collider;
             }
         }
     }
 
-    return { xCollision, yCollision, xCollisionSide, yCollisionSide, colliderEl };
-}
+    return { collision, collisionSide, colliderEl };
+};
 
-// Update the player position and handle collisions
-function updateDiverPosition() {
-    const speed = 0.008; // Movement speed
+const isPointInsideElement = (point, rect) => {
+    return (
+        point.x >= rect.left &&
+        point.x <= rect.right &&
+        point.y >= rect.top &&
+        point.y <= rect.bottom
+    );
+};
 
-    // Calculate new position to move the character based on input movement
-    const newPosX = (1 - speed) * parseFloat(sprite.style.left || sprite.offsetLeft) + speed * prevInputX;
-    const newPosY = (1 - speed) * parseFloat(sprite.style.top || sprite.offsetTop) + speed * prevInputY;
-    const dx = prevInputX - parseFloat(sprite.style.left || sprite.offsetLeft);
-    const dy = prevInputY - parseFloat(sprite.style.top || sprite.offsetTop);
+const updateDiverPosition = () => {
+    const speed = 0.008;
+    const dx = prevInputX - (parseFloat(sprite.style.left) || sprite.offsetLeft);
+    const dy = prevInputY - (parseFloat(sprite.style.top) || sprite.offsetTop);
     const angle = Math.atan2(dy, dx);
+    const newX = (1 - speed) * (parseFloat(sprite.style.left) || sprite.offsetLeft) + speed * prevInputX;
+    const newY = (1 - speed) * (parseFloat(sprite.style.top) || sprite.offsetTop) + speed * prevInputY;
 
-    // Set sprite rotation
-    sprite.style.transform = `translate(-50%, -50%) rotate(${angle}rad)`;
+    sprite.style.transform = `translate(-50%, -50%) rotate(${angle}rad)${(angle > Math.PI / 2 || angle < -Math.PI / 2) ? ' scaleY(-1)' : ''}`;
 
-    // Flip sprite to prevent them from swimming upside down.
-    if (angle > Math.PI / 2 || angle < -Math.PI / 2) {
-        sprite.style.transform += ' scaleY(-1)';
-    }
+    const { collision, collisionSide, colliderEl } = checkCollisions();
+    const isTrigger = colliderEl?.classList.contains('trigger');
+    let [adjustedX, adjustedY] = [newX, newY];
 
-    // Check if the player has collided and which side they collided on to move the player away from the collider.
-    const { xCollision, yCollision, xCollisionSide, yCollisionSide, colliderEl } = checkCollisions();
-    const isTrigger = colliderEl.classList.contains('trigger') ? true : false;
-    
-    let adjustedPosX = newPosX;
-    let adjustedPosY = newPosY;
+    adjustedX += collision.x && !isTrigger ? (collisionSide.x === 'left' ? 5 : -5) : 0;
+    adjustedY += collision.y && !isTrigger ? (collisionSide.y === 'top' ? 5 : -5) : 0;
 
-    if (xCollision && !isTrigger) {
-        adjustedPosX += xCollisionSide === 'left' ? 5 : -5; // Move the player 5 pixels away from the collider if they collide on x
-    }
+    const containerTop = main.getBoundingClientRect().top;
+    const gradientX = (adjustedX / window.innerWidth) * 100;
+    const gradientY = ((adjustedY + containerTop) / window.innerHeight) * 100;
 
-    if (yCollision && !isTrigger) {
-        adjustedPosY += yCollisionSide === 'top' ? 5 : -5; // Move the player 5 pixels away from the collider if they collide on Y
-    }
+    gradientContainer.style.background = `radial-gradient(circle at ${gradientX}% ${gradientY}%, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .7) 30%)`;
 
-    // Update sprite and playerbox position
-    sprite.style.left = adjustedPosX + 'px';
-    playerbox.style.left = adjustedPosX + 'px';
+    sprite.style.left = playerbox.style.left = adjustedX + 'px';
+    playerbox.style.top = sprite.style.top = (adjustedY < 150) ? '150px' : adjustedY + 'px';
 
-    // Adjust player position to prevent peaking over the water edge
-    if (adjustedPosY < 150) {
-        playerbox.style.top = '150px';
-        sprite.style.top = '150px';
-    } else {
-        playerbox.style.top = adjustedPosY + 'px';
-        sprite.style.top = adjustedPosY + 'px';
-    }
     requestAnimationFrame(updateDiverPosition, scrollInput());
-}
-
-main.addEventListener('mouseenter', () => {
-    mouseOverMain = true;
-});
-
-main.addEventListener('mouseleave', () => {
-    mouseOverMain = false;
-});
-
-// Event listener for mouse movement
-main.addEventListener('mousemove', (e) => {
-    handleMouseMove(e);
-});
-
-// Event listener for touch start
-main.addEventListener('touchstart', (e) => {
-    handleTouchMove(e);
-});
-
-// Event listener for touch move
-main.addEventListener('touchmove', (e) => {
-    handleTouchMove(e);
-});
-
-// Start updating player position
-updateDiverPosition();
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
-const tetraFish = document.getElementById('tetraFish');
+const tetraFish = document.querySelector('#tetraFish');
+const header = document.querySelector('#header');
+
+let mouseOverMain = false;
 
 const keyframes = [
     { backgroundPosition: `-205px 0px`, duration: 0.1, ease: "steps(1)" },
@@ -11,9 +14,59 @@ const keyframes = [
     { backgroundPosition: `6px -271px`, duration: 0.1, ease: "steps(1)" },
 ];
 
-tetraFish.addEventListener('animationiteration', () => {
-    tetraFish.classList.toggle('swim-back');
-});
+const scrollInput = () => {
+    if (!mouseOverMain) return;
 
-// Start sprite animation
+    const spriteCenterY = sprite.getBoundingClientRect().top + sprite.offsetHeight / 2;
+    const windowHeight = window.innerHeight;
+    const scrollThreshold = windowHeight * 0.4;
+
+    const scrollAmount = spriteCenterY < scrollThreshold ? -3 : (spriteCenterY > windowHeight - scrollThreshold ? 3 : 0);
+    window.scrollBy(0, scrollAmount);
+};
+
+const debounce = (callback, delay) => {
+    let timer;
+    return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback.apply(context, args);
+      }, delay);
+    };
+  };
+
+const throttle = (callback, delay) => {
+    let previousCall = new Date().getTime();
+    return function() {
+      const time = new Date().getTime();
+      if ((time - previousCall) >= delay) {
+        previousCall = time;
+        callback.apply(null, arguments);
+      }
+    };
+  };
+
+const setScale = () => {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const scale = Math.min(screenWidth, screenHeight) * 0.10 / 100;
+
+    sprite.style.scale = scale;
+    playerbox.style.scale = scale;
+}
+
+main.addEventListener('mousemove', throttle(handleMouseMove, 10));
+main.addEventListener('touchstart', throttle(handleMouseMove, 10));
+main.addEventListener('touchmove', throttle(handleMouseMove, 10));
+
+main.addEventListener('mouseenter', () => throttle(mouseOverMain = true, 50));
+main.addEventListener('mouseleave', () => throttle(mouseOverMain = false, 50));
+
+window.addEventListener('resize', debounce(setScale, 250));
+
 gsap.fromTo(sprite, { keyframes: keyframes }, { keyframes: keyframes, repeat: -1 });
+
+setScale();
+updateDiverPosition();
